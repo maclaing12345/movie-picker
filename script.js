e8c809ac06e26a977e214362999208335552201a
wyattsm <wsmyth28@students.stab.org>
2024-11-07 10:12:57 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 function movie() {
-    var genre = document.getElementById(genre)
-    var age = document.getElementById(age)
+    var genre = document.getElementById("genre").value;
+    var age = document.getElementById("age").value;
+var output=document.getElementById("output").value;
 
-    if (age=18+ && genre = horror) {
-        console.log(scream)
-        console.log(smile)
-        console.log(scream2)
+    if (age=="18+" && genre == "Horror") {
+        output=("Scream")
+        console.log("Smile")
+        console.log("Scream 2")
     }
-
+    else if(age=="0-17" && genre=="Horror"){
+        console.log("Coraline")
+        console.log("Goosebumps")
+        console.log("Nightmare Before Christmas")
+    }
+    if (age=="18+" && genre == "Romance") {
+        console.log("50 Shades of Gray")
+        console.log("Blue Lagoon")
+        console.log("The Proposal")
+    }
+    if (age=="0-17" && genre == "Romance") {
+        console.log("Cinderella Story")
+        console.log("My Girl")
+        console.log("Anyone But You")
+    }
+    if (age=="18+" && genre == "Comedy") {
+        console.log("Superbad")
+        console.log("Good Boys")
+        console.log("Hangover")
+    }
+    if (age=="0-17" && genre == "Comedy") {
+        console.log("Shrek")
+        console.log("Home Alone")
+        console.log("Elf")
+    }
 }
