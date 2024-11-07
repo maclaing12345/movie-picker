86df3abb50f5d668d4a68d79b8e8e93eb8599290
maclaing12345 <mlaing28@students.stab.org>
2024-11-07 09:38:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function movie() {
+    var genre = document.getElementById(genre)
+    var age = document.getElementById(age)
+
+    if (age=18+ && genre = horror) {
+        console.log(scream)
+        console.log(smile)
+        console.log(scream2)
+    }
+
+}
