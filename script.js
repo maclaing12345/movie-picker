13661c91ae17963829a731c0c64d177a3823dfe9
wyattsm <wsmyth28@students.stab.org>
2024-11-08 20:47:11 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-function movie() {
-    var genre = document.getElementById("genre").value
-    var age = document.getElementById("age").value
-
+function movie(age, genre) {
+    //var genre = document.getElementById("genre").value
+    //var age = document.getElementById("age").value
+    console.log(age);
+    console.log(genre);
+    //the results variable records the output
+    //Each of the if statements decide the output based on the input
     var results;
-   if (age=="18+" && genre == "Horror") {
+    if (age=="Age" || genre=="Genre"){
+        results=""
+    }
+   if (age>18 && genre == "Horror") {
         results = "scream <br> smile <br> Scream 2";
       
     }
-     if(age=="0-17" && genre == "Horror"){
+     if(age<18 && genre == "Horror"){
       results = "Coraline <br> goosbumps <br> Nightmere before christmas";
 
     }
-    if (age=="18+" && genre == "Romance") {
+    if (age>18 && genre == "Romance") {
         results = "50 shades of grey <br> Bluelagoon <br> The Proposal";
         
     }
-     if(age=="0-17" && genre == "Romance"){
+     if(age<18 && genre == "Romance"){
         results = "Cinderella story <br> My Girl <br> Anyone but you"
      }
-    if (age=="18+" && genre == "Comedy") {
+    if (age>18 && genre == "Comedy") {
         results = "Superbad <br> Hangover <br> Good Boys"
     
     }
-     if(age=="0-17" && genre == "Comedy"){
+     if(age<18 && genre == "Comedy"){
        results = "Shrek <br> Home Alone <br> Elf"
     }
 
-    document.getElementById("output").innerHTML = results;
+ return results   
+
 }
+//this function uses the results saved from the first function and displays them
+function updateScreen(){
+    document.getElementById("output").innerHTML = movie(document.getElementById("age").value, document.getElementById("genre").value);
+}
+
